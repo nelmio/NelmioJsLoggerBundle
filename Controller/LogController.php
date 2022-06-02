8bc059b83b8fcae31a00910d7b247414e7497cf3
Tobias Bönner <boenner@users.noreply.github.com>
2022-06-02 14:35:22 +0200

    Change parameter handling for logging via GET
    This fixes #44.

--- Controller/LogController.php
+++ Controller/LogController.php
@@ -18,10 +18,10 @@ class LogController
     public function createAction(Request $request)
     {
         if(Request::METHOD_GET === $request->getMethod()){
-            $level = (string) $request->query->get('level');
-            $message = (string) $request->query->get('msg');
-            $context = (array) $request->query->get('context', array());
-
+            $parameters = $request->query->all();
+            $level = $parameters['level'] ?? '';
+            $message = $parameters['msg'] ?? '';
+            $context = $parameters['context'] ?? [];
         } else {
             $postData = json_decode($request->getContent());
             $level = (string) $postData->level;
